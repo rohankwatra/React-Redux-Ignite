ab72195cbeeb8072ae4e602e9fc02369473c8f5a
Rohan Kwatra <rohankwatra761@gmail.com>
2022-12-25 22:56:12 +0530

    Styling Up The Cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,53 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+//Redux
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
+//Components
+import Game from "../components/Game";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 function Home(){
     const dispatch = useDispatch();
     //Fetch Games
   useEffect(() => {
     dispatch(loadGames());
-  });
+  },[dispatch]);
+
+  // Get the data from Redux State 
+  const {popular,newGames,upcoming} = useSelector((state)=> state.games);
+
   return (
-    <h1>Home</h1>
+    <GamesList>
+    <h2>Upcoming Games</h2>
+    <Games>
+      {upcoming.map(game =>(
+        <Game 
+        name={game.name} 
+        released={game.released} 
+        id={game.id}
+        image={game.background_image}
+        key={game.id}/>
+      ))}
+    </Games>
+    </GamesList>
   )
 }
+
+const GamesList = styled(motion.div)`
+  padding:0rem 5rem;
+  h2{
+    padding:5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height:80vh;
+  display:grid;
+  grid-template-columns:repeat(auto-fit,minmax(500px,1fr));
+  grid-column-gap:3rem;
+  grid-row-gap:3rem;
+`;
 
 export default Home;
