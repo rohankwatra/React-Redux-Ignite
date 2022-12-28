ba2546d5833e9014541b0893a81461ecfef65c3b
Rohan Kwatra <rohankwatra761@gmail.com>
2022-12-28 22:59:02 +0530

    Adding Game Detail Data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesAction";
 //Components
 import Game from "../components/Game";
+import GameDetail from "../components/GameDetail";
 //Styling and Animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
@@ -20,6 +21,7 @@ function Home(){
 
   return (
     <GamesList>
+      <GameDetail/>
     <h2>Upcoming Games</h2>
     <Games>
       {upcoming.map(game =>(
