fb2a87fc0c23ad6c0595e608db7f13fd0d58f77b
Rohan Kwatra <rohankwatra761@gmail.com>
2023-01-09 22:19:25 +0530

    Adding Icons

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,7 +6,13 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 //Redux
 import { useSelector } from "react-redux";
-
+//Images
+import playstation from "../img/playstation.svg"
+import steam from "../img/steam.svg"
+import xbox from "../img/xbox.svg"
+import nintendo from "../img/nintendo.svg"
+import apple from "../img/apple.svg"
+import gamepad from "../img/gamepad.svg"
 const GameDetail = ({pathId})=>{
   const history = useHistory();
   //Exit Detail
@@ -17,6 +23,25 @@ const GameDetail = ({pathId})=>{
       history.push('/');
     }
   } 
+
+  //get platform images
+  const getplatform =(platform)=>{
+    switch(platform){
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad; 
+
+    }
+  }
   
   //Data
     const {screen,game,isLoading} = useSelector((state)=>state.detail);
@@ -34,7 +59,7 @@ const GameDetail = ({pathId})=>{
                         <h3>Platforms</h3>
                         <Platforms>
                             {game?.platforms?.map(data=>(
-                                <h3 key={data?.platform?.id}>{data?.platform?.name}</h3>
+                                <img key={data?.platform?.id} src={getplatform(data.platform.name)} alt={data.platform.name} >{data?.platform?.name}</img>
                             ))}
                         </Platforms>
                     </Info>
@@ -65,6 +90,7 @@ const CardShadow = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
+  z-index:5;
   &::-webkit-scrollbar {
     width: 0.5rem;
   }
@@ -84,6 +110,7 @@ const Detail = styled(motion.div)`
   position: absolute;
   left: 10%;
   color: black;
+  z-index:10;
   img {
     width: 100%;
   }
